7444f0dde76e24e22f7928a22091e56298d06c13
lll-Mike-lll <37527876+lll-Mike-lll@users.noreply.github.com>
2018-04-02 12:52:16 +0700

    m1

---
+++ test_01.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 20 10:35:08 2018
+
+@author: wuttinun_r
+"""
+
+import pandas as pd
+url = "http://www.panphol.com/data/page/stockprice/ADVANC#"
+data1 = pd.read_html(url);
+#print(data1[0])
+print(data1[0].iloc[0,1])
